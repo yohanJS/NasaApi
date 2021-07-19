3fa3eba077bc70fbc28c543cb8e435b2ceceb035
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2021-07-18 23:09:57 -0400

    script added, css added, and html content added. Still needs work

--- js/site.js
+++ js/site.js
@@ -1,4 +1,20 @@
+"use strict"
 
-let helloWorld = () => {
-    alert("Hello World");
+
+let content = document.getElementById("content");
+
+
+function fetchData() {
+  const MyKEY = "dTs4iAhgxeAdnEtRhKiEhIYPt4mwptPjrofcSepz";
+  let myData = fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=${MyKEY}`)
+      .then(response => response.json())
+      .then(data => {
+        content.innerHTML += data.photos[0].camera.full_name;
+        content.innerHTML += `<br>Date on earth ${data.photos[0].earth_date} when the photo was taken`;
+        content.innerHTML += `<img src="${data.photos[0].img_src}" id="my-image">`;
+      });
+}
+
+window.onload = function() {
+  fetchData();
 }
